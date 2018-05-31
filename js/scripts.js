15233edb8e1a54b8c09e54ca0c267f13b384a51a
Brandon Betts and Craig Wann <craig@>
2018-05-31 15:46:51 -0700

    add triangle and function js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
-  $("#celebrity").submit(function(event) {
-    var inputName = $("input#name").val();
-    var inputAge = $("input#favFood").val();
+  $("#sides").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
 
-
+    if (side1 === side2 && side1 === side3 && side2 === side3) {
+      $(".eq").show();
+      $(".iso").hide();
+      $(".sca").hide();
+      $(".non").hide();
+    } if (side1 === side2 || side1 === side3 || side2 === side3) {
+      $(".eq").hide();
+      $(".iso").show();
+      $(".sca").hide();
+      $(".non").hide();
+    } else if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2) {
+      $(".eq").hide();
+      $(".iso").hide();
+      $(".sca").hide();
+      $(".non").show();
+    } else {
+      $(".eq").hide();
+      $(".iso").hide();
+      $(".sca").show();
+      $(".non").hide();
+    }
     event.preventDefault();
   });
 });
